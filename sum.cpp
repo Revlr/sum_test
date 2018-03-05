729e92862c246bda075b3c3050aa810926e22af1
root <jjh8501@gmail.com>
2018-03-05 20:03:43 +0900

    Modify sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,11 +1,6 @@
 #include "sum.h"
 
 int sum(int begin, int end){
-	int total=0;
-	int i;
-	for(i = begin; i <= end; i++){
-		total = total + i;
-	}
-	return  total;
+	return end*(end+1)/2 - begin*(begin-1)/2;
 }
 
